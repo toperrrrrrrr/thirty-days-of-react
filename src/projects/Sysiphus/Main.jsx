24ddea7416829a9b65ac4cee7f83f123ba8a97a6
Nori Lacsina <toperrrrrrrr@gmail.com>
2024-05-09 18:55:29 +0800

    Background, sections and navbar

--- src/projects/Sysiphus/Main.jsx
+++ src/projects/Sysiphus/Main.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 import "./res/styles.css";
-import Mountain from "./res/imgs/BG.svg";
-import Pushing from "./res/imgs/Pushing.svg";
 
 export default function Main() {
    return (
       <>
-         <div className="hero">
-            <div className="bg">
-               <img src={Mountain} alt="Mountain" className="imgs bg-mountain" />
-               <img src={Pushing} alt="Pushing" className="imgs bg-push" />
-         
-            </div>
-            
-            <div className="container">
-              <div className="row">
-                <div className="col-6">
-                  <h1 className="title">Sisyphus</h1>
-                </div>
-                <div className="col-6">
-                <h1 className="title">Sisyphus</h1>
-                </div>
-              </div>
+         <div>
+            <div className="container-fluid">
+               <div className="row">
+                  <div className="col-3 nav-center">
+                     <h1 className="nav-text">Sisyphus</h1>
+                  </div>
+                  <div className="col-3 nav-center">
+                     <h1 className="nav-text">Sisyphus</h1>
+                  </div>
+                  <div className="col-3 nav-center">
+                     <h1 className="nav-text">Sisyphus</h1>
+                  </div>
+                  <div className="col-3 nav-center">
+                     <h1 className="nav-text">Sisyphus</h1>
+                  </div>
+               </div>
             </div>
          </div>
+         <div className="hero"></div>
 
-         <div className="section1"></div>
+         <div className="section1">
+            <div className="container">
+               <div className="row">
+                  <div className="col-6">
+                     <h1 className="title">Sisyphus</h1>
+                  </div>
+                  <div className="col-6">
+                     <h1 className="title">Sisyphus</h1>
+                  </div>
+               </div>
+            </div>
+         </div>
       </>
    );
 }
